a69720a7e37cd58b008daa9ed3de8bf26b0f46af
John Hebert <jlhebert2011@gmail.com>
2017-01-26 19:26:09 -0500

    Increased fan on time delay

--- KittyControl.py
+++ KittyControl.py
@@ -3,7 +3,7 @@
 # Uses PIR sensor to monitor presence of cat in litter box and
 # turns on exhaust fan and lights.  Fan remains on for On_Time after
 # cat presence is no longer detected.
-# Lights are ramped up with a PWM to prevent startling the kittie.
+# Lights are ramped up with a PWM to prevent startling the kitty.
 #
 # John Hebert
 # 2017
@@ -16,7 +16,7 @@ import time
 PIR_Sensor = 11                     # Motion sensor input
 LED_Lights = 3                      # PWM output for LEDs
 Fan = 7                             # fan relay output
-On_Time = 30                        # fan/lights delay seconds
+On_Time = 45                        # fan/lights delay seconds
 
 # set up GPIO I/O pins
 GPIO.setwarnings(False)
@@ -43,7 +43,6 @@ while True:
             Prev_Value = 0
             
     elif State == 1:            # STATE 1: ramp lights up and turn fan on
-        print ("on")
         GPIO.output(Fan, 1)     # fan on
         for j in range(50):     # ramp the PWM output
             k = j * 2
@@ -55,14 +54,12 @@ while True:
         GPIO.output(Fan, 0)     # fan off
         PWM.ChangeDutyCycle(0)  # lights off
         State = 0
-        print ("off")
         
     elif State == 3:            # STATE 3: time on delay
         time.sleep(1)           # one second
         Delay_Counter = Delay_Counter + 1
         if Sensor == 1:         # reset on time counter if sensor is triggered again
             Delay_Counter = 0
-            print ("retrigger")
         if Delay_Counter >= On_Time:
             State = 0
             Delay_Counter = 0
